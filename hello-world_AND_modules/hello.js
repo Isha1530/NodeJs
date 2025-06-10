966ffd28c3254fb498347da7b0eda1f605d3e7a1
Isha1530 <147532644+Isha1530@users.noreply.github.com>
2025-06-10 12:54:36 +0530

    Add files via upload

---
+++ hello-world_AND_modules/hello.js
@@ -0,0 +1,5 @@
+const math = require('./math');
+console.log("Hello JS")
+
+console.log("value is " ,math.add(10, 20));
+console.log("value is " ,math.sub(20, 10));
---
+++ hello-world_AND_modules/math.js
@@ -0,0 +1,19 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// module.exports = {
+//   addfn: add,
+//   subfn: sub,
+// };
+
+exports.add = (a, b) => {
+  return a + b;
+}
+exports.sub = (a, b) => {
+  return a - b;
+}
